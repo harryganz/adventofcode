c8585866f5273f40d228b1502efd441c16747384
Harry Ganz <harryyehudah.ganz@gartner.com>
2019-12-12 13:19:59 -0500

    Implements day3 part1

--- day3/main.go
+++ day3/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/harryganz/adventofcode/utils"
 	"github.com/harryganz/adventofcode/wires"
 	"os"
+	"strings"
 )
 
 func main() {
 	GOPATH := os.Getenv("GOPATH")
 	filepath := GOPATH + "/src/github.com/harryganz/adventofcode/data/day3.txt"
 
-	inputStrings, err := utils.ScanFile(filepath, utils.SplitCommas)
+	inputLines, err := utils.ScanFile(filepath, bufio.ScanLines)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(inputStrings[0:3])
+	line1 := inputLines[0]
+	line2 := inputLines[1]
 
 	w1 := wires.New()
-	w1.AddSegment("R", 2)
-	w1.AddSegment("D", 1)
-	w1.AddSegment("L", 3)
-	w1.AddSegment("U", 1)
+	w2 := wires.New()
+
+	w1.AddSegments(strings.Split(line1, ","))
+	w2.AddSegments(strings.Split(line2, ","))
+
+	distance := wires.ClosestIntersectionDistance(w1.GetIntersections(w2))
+
+	fmt.Printf("Closest intersection to port = %d\n", distance)
 }

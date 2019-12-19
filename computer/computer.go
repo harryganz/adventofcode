bcf143b6ad71008a4c057bfaca22c4b093e0c2e1
Harry Ganz <harryyehudah.ganz@gartner.com>
2019-12-19 15:45:48 -0500

    Revert "Begins refactoring of RunProgram"
    This reverts commit 1c50169d4113ace4d37eec76f842e5a8777c1c74.

--- computer/computer.go
+++ computer/computer.go
@@ -5,43 +5,28 @@ import "errors"
 func RunProgram(input []int) ([]int, error) {
 	for pos := 0; pos < len(input) && input[pos] != 99; pos += 4 {
 		instruction := parseInstruction(input[pos])
-
-		out, err := execute(input, instruction, input[pos+1:pos+4])
+		x1 := input[input[pos+1]]
+		x2 := input[input[pos+2]]
+		loc := input[pos+3]
+		out, err := execute(instruction[0], x1, x2)
 		if err != nil {
 			return input, err
 		}
-		input = out
+		input[loc] = out
 	}
 
 	return input, nil
 }
 
-func execute(input, instruction, params []int) ([]int, error) {
-	paramValues := make([]int, len(params))
-	for i, param := range params {
-		switch mode := instruction[i+1]; mode {
-		case 0:
-			paramValues[i] = input[param]
-		case 1:
-			paramValues[i] = param
-		}
-	}
-	switch opCode := instruction[0]; opCode {
+func execute(opcode, x1, x2 int) (int, error) {
+	switch opcode {
 	case 1:
-		loc := paramValues[2]
-		x := paramValues[0]
-		y := paramValues[1]
-		input[loc] = x + y
+		return x1 + x2, nil
 	case 2:
-		loc := paramValues[2]
-		x := paramValues[0]
-		y := paramValues[1]
-		input[loc] = x * y
+		return x1 * x2, nil
 	default:
-		return []int{}, errors.New("Unknown opcode")
+		return 0, errors.New("Unknown opcode")
 	}
-
-	return input, nil
 }
 
 func parseInstruction(instruction int) []int {

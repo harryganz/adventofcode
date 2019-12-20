cf30497d4aa036f7cdb7d120b4186ac581380fa0
Harry Ganz <harryyehudah.ganz@gartner.com>
2019-12-20 12:35:15 -0500

    Modifies execute to mutate input in place

--- computer/computer.go
+++ computer/computer.go
@@ -5,28 +5,28 @@ import "errors"
 func RunProgram(input []int) ([]int, error) {
 	for pos := 0; pos < len(input) && input[pos] != 99; pos += 4 {
 		instruction := parseInstruction(input[pos])
-		loc := input[pos+3]
-		out, err := execute(input, instruction, input[pos+1:pos+3])
+		err := execute(input, instruction, input[pos+1:pos+4])
 		if err != nil {
 			return input, err
 		}
-		input[loc] = out
 	}
 
 	return input, nil
 }
 
-func execute(input, instruction, params []int) (int, error) {
+func execute(input, instruction, params []int) error {
 	switch opcode := instruction[0]; opcode {
 	case 1:
 		x1, x2 := input[params[0]], input[params[1]]
-		return x1 + x2, nil
+		input[params[2]] = x1 + x2
 	case 2:
 		x1, x2 := input[params[0]], input[params[1]]
-		return x1 * x2, nil
+		input[params[2]] = x1 * x2
 	default:
-		return 0, errors.New("Unknown opcode")
+		return errors.New("Unknown opcode")
 	}
+
+	return nil
 }
 
 func parseInstruction(instruction int) []int {
